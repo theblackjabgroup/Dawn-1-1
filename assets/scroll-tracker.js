456b85a994b82889a3befb16a95487ab1938932a
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-08-29 10:33:54 +0530

    Add files via upload

---
+++ assets/scroll-tracker.js
@@ -0,0 +1,32 @@
+let scrollDistance = parseFloat(localStorage.getItem('scrollDistance')) || "";
+let prevScrollPosition = { x: window.scrollX, y: window.scrollY };
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('scrollDistance').textContent = scrollDistance.toFixed(2);
+  document.getElementById('scrollDistanceWidget').textContent =  scrollDistance.toFixed(3)
+})
+
+
+const updateElements = (distance) => {
+  document.getElementById('scrollDistance').textContent = distance.toFixed(2);
+  document.getElementById('scrollDistanceWidget').textContent = distance.toFixed(3)
+};
+
+const updateScrollDistance = () => {
+  const currentScrollPosition = { x: window.scrollX, y: window.scrollY };
+  const deltaX = currentScrollPosition.x - prevScrollPosition.x;
+  const deltaY = currentScrollPosition.y - prevScrollPosition.y;
+  const incrementalDistance = Math.sqrt(deltaX ** 2 + deltaY ** 2) / 1000000;
+  scrollDistance += incrementalDistance;
+  localStorage.setItem('scrollDistance', scrollDistance);
+
+  updateElements(scrollDistance);
+
+  prevScrollPosition = currentScrollPosition;
+};
+
+window.addEventListener('scroll', updateScrollDistance);
+
+document.getElementById('scroll-to-bottom').addEventListener('click', () => {
+  window.scrollTo({top: document.body.scrollHeight, behavior:'smooth'})
+})
